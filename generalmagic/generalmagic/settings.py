e513180054d5502f41afb1da0262bfcb8ddf7e70
Sai Manoj Karanam <151407816+manojkaranamm-stack@users.noreply.github.com>
2024-05-27 16:48:38 -0400

    added hosts

--- generalmagic/generalmagic/settings.py
+++ generalmagic/generalmagic/settings.py
@@ -21,11 +21,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'django-insecure-arqj3$j=o7wqj=5jq2ci1ruh4)1x^--_h)==n$b%=^w2r!bj*='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://general-magic.onrender.com']
 
 
 # Application definition
